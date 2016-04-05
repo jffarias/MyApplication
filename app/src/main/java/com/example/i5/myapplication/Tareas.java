b4eedff8141a5ccef963d218554f4786be05949d
jffarias <jesusflr4@gmail.com>
2016-04-05 05:06:38 -0500

    ActionBar Eliminar Registros

--- app/src/main/java/com/example/i5/myapplication/Tareas.java
+++ app/src/main/java/com/example/i5/myapplication/Tareas.java
@@ -8,6 +8,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,7 +19,10 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class Tareas extends AppCompatActivity {
+    String[] Arreglo;
     ListView lvTareas;
+    protected Object noActionMode;
+    public int selectedItem=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +42,21 @@ public class Tareas extends AppCompatActivity {
             }
         });
         CargarDatos();
-        //CargarDatos();
+        lvTareas = (ListView) findViewById(R.id.listaTareas);
+        lvTareas.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        lvTareas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+                if(noActionMode != null){
+                    return false;
+                }
+                selectedItem = position;
+                //menuTarea
+                noActionMode = Tareas.this.startActionMode(menuTarea);
+                view.setSelected(true);
+                return true;
+            }
+        });
     }
 
     public void CargarDatos(){
@@ -46,7 +67,7 @@ public class Tareas extends AppCompatActivity {
             Cursor c = db.rawQuery("SELECT * FROM Tareas", null);
             int Cantidad = c.getCount();
             int i=0;
-            String[] Arreglo = new String[Cantidad];
+            Arreglo = new String[Cantidad];
             if(c.moveToFirst()){
                 do{
                     String Linea = c.getInt(0) + " " + c.getString(1);
@@ -60,14 +81,52 @@ public class Tareas extends AppCompatActivity {
                     android.R.layout.simple_list_item_1, Arreglo);
             ListView lvTareas = (ListView) findViewById(R.id.listaTareas);
             lvTareas.setAdapter(Adapter);
+        }
+    }
 
-            lvTareas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(getApplicationContext(), String.valueOf(position),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
+    private ActionMode.Callback menuTarea = new ActionMode.Callback(){
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.menu_tareas, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch(item.getItemId()){
+                case R.id.menu_borrar:
+                    Borrar();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            noActionMode = null;
+            selectedItem = -1;
+        }
+    };
+
+    private void Borrar(){
+        DBhelper dbhelper = new DBhelper(this, "DBAPP", null, 1);
+        SQLiteDatabase db = dbhelper.getReadableDatabase();
+        int id= Integer.parseInt(Arreglo[selectedItem].split(" ")[0]);
+        if(db != null){
+            long res = db.delete("Tareas", "Id="+id, null);
+            if(res > 0){
+                Toast.makeText(getApplicationContext(), "Tarea Eliminada "+id, Toast.LENGTH_SHORT).show();
+                CargarDatos();
+            }
         }
     }
 }
